dffb4d813b874cbe0868d000554c53b531e557df
Tim Wüllner <tim.wuellner@openknowledge.de>
2022-08-08 15:02:57 +0200

    add touch listener

--- camera.js
+++ camera.js
@@ -1,12 +1,15 @@
+
+const photoButton = document.querySelector("#takePhoto");
+const videoOutput = document.querySelector("#canvasOutput");
+const video = document.querySelector("#videoElement");
+video.setAttribute("playsinline", "");
+video.setAttribute("autoplay", "");
+video.setAttribute("muted", "");
+video.style.width = "200px";
+video.style.height = "200px";
+
 function startCamera(faceCascade) {
-    const photoButton = document.querySelector("#takePhoto");
-    const videoOutput = document.querySelector("#canvasOutput");
-    const video = document.querySelector("#videoElement");
-    video.setAttribute("playsinline", "");
-    video.setAttribute("autoplay", "");
-    video.setAttribute("muted", "");
-    video.style.width = "200px";
-    video.style.height = "200px";
+
 
     if (navigator.mediaDevices.getUserMedia) {
 
@@ -27,11 +30,19 @@ function startCamera(faceCascade) {
             });
 
         photoButton.addEventListener('click', async function () {
-            const context = videoOutput.getContext('2d');
-            context.drawImage(video, 0, 0, videoOutput.width, videoOutput.height);
-            videoOutput.toDataURL('image/png')
-            let mat = cv.imread("canvasOutput");
-            const imgWithFace = await detectHaarFace(mat, faceCascade)
-            cv.imshow('canvasOutput', imgWithFace);
+            await takePhoto(faceCascade)
+        });
+
+        photoButton.addEventListener('touchstart', async function () {
+            await takePhoto(faceCascade)
         });
     }}
+
+async function takePhoto(faceCascade) {
+    const context = videoOutput.getContext('2d');
+    context.drawImage(video, 0, 0, videoOutput.width, videoOutput.height);
+    videoOutput.toDataURL('image/png')
+    let mat = cv.imread("canvasOutput");
+    const imgWithFace = await detectHaarFace(mat, faceCascade)
+    cv.imshow('canvasOutput', imgWithFace);
+}
