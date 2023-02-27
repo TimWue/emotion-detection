5f0abd939522dfd9904677cbc09ef16af018aa2e
Tim Wuellner <tim.wuellner@openknowledge.de>
2023-02-27 23:32:22 +0100

    create gh-pages branch

--- scripts/github-pages-deploy.sh
+++ scripts/github-pages-deploy.sh
@@ -20,5 +20,5 @@ git commit -m 'deploy'
   # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
   # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:timwue/serve.cnn.git main:gh-pages
+git push -f https://github.com/TimWue/serve.cnn.git main:gh-pages
 cd -
